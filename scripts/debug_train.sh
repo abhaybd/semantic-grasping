f879c94dc2a590fd29e33455abe8ee45b9b6a0e2
Abhay D <a.deshpande012@gmail.com>
2025-04-02 14:09:38 -0700

    Update config and launch scripts

--- scripts/debug_train.sh
+++ scripts/debug_train.sh
@@ -1,6 +1,7 @@
 beaker session create -w ai2/abhayd --budget ai2/prior \
     --gpus 4 \
-    --shared-memory 64GiB \
-    --mount src=beaker,ref=abhayd/dataset_test_0314_1317,dst=/train_data/dataset \
-    --mount src=beaker,ref=abhayd/obsgen_0311_1420,dst=/train_data/data \
+    --shared-memory 128GiB \
+    --secret-env WANDB_API_KEY=WANDB_API_KEY \
+    --mount src=beaker,ref=abhayd/dataset_0324_2125,dst=/train_data/dataset \
+    --mount src=beaker,ref=abhayd/obsgen_0324_1943,dst=/train_data/data \
     --bare
--- scripts/launch_regression.sh
+++ scripts/launch_regression.sh
@@ -4,7 +4,7 @@ if [ $# -ne 1 ]; then
 fi
 
 NAME=$1
-DATASET_NAME=dataset_0323_1529
+DATASET_NAME=dataset_0401_2018
 WORLD_SIZE=4
 
 OBS_DATASET=$(python -m semantic_grasping_datagen.get_data_for_dataset abhayd/${DATASET_NAME})
@@ -23,9 +23,8 @@ gantry run -w ai2/abhayd -b ai2/prior \
     --priority normal \
     --cluster ai2/augusta-google-1 \
     --cluster ai2/jupiter-cirrascale-2 \
-    --cluster ai2/neptune-cirrascale \
+    --cluster ai2/saturn-cirrascale \
     --cluster ai2/ceres-cirrascale \
-    --cluster ai2/prior-elanding \
     --shared-memory 128GiB \
     --allow-dirty \
     -- \
