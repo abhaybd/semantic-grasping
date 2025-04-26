0f60280f4d536bf86c06b59645f57717bbe40de5
Abhay D <a.deshpande012@gmail.com>
2025-04-26 16:51:36 -0700

    Fix task name

--- scripts/launch_tg_eval.sh
+++ scripts/launch_tg_eval.sh
@@ -4,6 +4,7 @@ if [ "$#" -ne 1 ]; then
 fi
 
 MODEL=$1
+TIMESTAMP=$(date +%m%d_%H%M)
 
 if [ "$MODEL" = "graspmolmo" ]; then
     MODEL_ARGS="eval_model=graspmolmo eval_model.ckpt_dir=/weka/oe-training-default/roseh/mm_olmo/robomolmo_checkpoints/graspmolmo_cotraining_06_evals_graspmolmo-focused_20250424_072247/step9000-unsharded"
@@ -17,8 +18,8 @@ fi
 SPLITS="o t"
 for split in $SPLITS; do
     gantry run -w ai2/abhayd -b ai2/prior \
-        --name "tg_eval_${MODEL}_${split}" \
-        --task-name "tg_eval_${MODEL}_${split}" \
+        --name "tg_eval_${MODEL}_${split}_${TIMESTAMP}" \
+        --task-name "tg_eval_${MODEL}_${split}_${TIMESTAMP}" \
         --env-secret GITHUB_TOKEN=GITHUB_TOKEN \
         --dataset-secret SSH_KEY:/root/.ssh/id_ed25519 \
         --beaker-image ai2/cuda11.8-dev-ubuntu20.04 \
